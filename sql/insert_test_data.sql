dd915d4c1ff2a6520773c3e063d50ec508d34927
Spatchy <spatchy@spatchy.net>
2023-10-22 17:21:24 +0100

    Add SQL to create tables and insert dummy data

---
+++ sql/insert_test_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO Services (service)
+VALUES ('youtube_publish');
+
+INSERT INTO Activities (service, published_date, title, url)
+VALUES ('youtube_publish', '2023-10-22 12:00:00', 'Dummy Title 1', 'https://example.com/video1'),
+       ('youtube_publish', '2023-10-22 13:00:00', 'Dummy Title 2', 'https://example.com/video2'),
+       ('youtube_publish', '2023-10-22 14:00:00', 'Dummy Title 3', 'https://example.com/video3');
